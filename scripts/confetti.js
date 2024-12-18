bdd405b2d9e66594907ac5ac653309508ca5da4b
paradaise <paradaiseru@gmail.com>
2024-12-18 11:51:13 +0300

    fix confetti

--- scripts/confetti.js
+++ scripts/confetti.js
@@ -9,17 +9,20 @@ function createConfetti(numConfetti) {
       confetti.classList.add('confetti');
       confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
       confetti.style.left = `${Math.random() * 100}vw`;
-      confetti.style.animationDelay = `${Math.random() * 3}s`;
+      confetti.style.animationDelay = `${Math.random() * 0.5}s`;
       confettiContainer.appendChild(confetti);
     }
   }
-  
+
+
   function showConfetti() {
     const confettiElements = document.querySelectorAll('.confetti');
     confettiElements.forEach(confetti => {
       confetti.style.display = 'block';
+      confetti.style.transform = `translateY(${Math.random() * -100}vh)`; 
     });
   }
+  
   
   function hideConfetti() {
     const confettiElements = document.querySelectorAll('.confetti');
